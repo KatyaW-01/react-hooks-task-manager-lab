687c02c698f7a322426a4e1f645514bdf5e6831b
Katya Weicht <katya.weicht@gmail.com>
2025-06-12 15:57:56 -0600

    replace state with context

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,6 +1,21 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const TaskContext = createContext();
 
 export function TaskProvider({ children }) {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+      fetch('http://localhost:6001/tasks')
+      .then(r=>r.json())
+      .then(data=>setTasks(data))
+      
+    }, []);
+
+    return (
+      <TaskContext.Provider value={{tasks,setTasks}} >
+        {children}
+      </TaskContext.Provider>
+    )
+
 }
