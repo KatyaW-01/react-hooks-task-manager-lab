d782aa7c6587c8338a7c545a5849c61af6fa9d1e
Katya Weicht <katya.weicht@gmail.com>
2025-06-13 17:31:37 -0600

    Implement useId on form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,8 +3,9 @@ import { TaskContext } from "../context/TaskContext";
 
 function TaskForm() {
   const [taskName, setTaskName] = useState("");
-
   const {addTask} = useContext(TaskContext)
+
+  const id = useId()
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -21,8 +22,9 @@ function TaskForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>New Task:</label>
+      <label htmlFor={id}>New Task:</label>
       <input
+        id={id}
         type="text"
         value={taskName}
         onChange={(e) => setTaskName(e.target.value)}
